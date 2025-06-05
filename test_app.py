51aff11ed2605166e7691f16e71458a7c2281f1f
varad <varadpk20@gmail.com>
2025-06-05 12:13:31 +0530

    same response in test

--- test_app.py
+++ test_app.py
@@ -4,4 +4,5 @@ def test_home():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data == b"Success"
+    # checks if the response data matches the expected output
+    assert response.data == b"Deployment of flask web app using Docker and Github Actions"
